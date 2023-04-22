844f728c006c35b337398bbaadbf99cece47cbb1
Федоренко Володимир <volodymyr.fedorenko@nure.ua>
2023-04-22 22:00:20 +0300

    Complete Task1

---
+++ Homework2/Homework2/Task1.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+
+namespace Homework2
+{
+    public class Task1 : ITask
+    {
+        public void Demo()
+        {
+            // Generate input data
+            Console.WriteLine("Task1 Demonstration");
+            var input = new List<string>
+            {
+                null,
+                "",
+                "Hello, world!",
+                "C# language first appeared in 2000, which was 23 years ago."
+            };
+
+            int sum = 0;
+            int maxDigit = 0;
+            foreach (var item in input)
+            {
+                // Print input data
+                if(item == null)
+                {
+                    Console.WriteLine("Input: null");
+                }
+                else
+                {
+                    Console.WriteLine($"Input: '{item}'");
+                }
+
+                // Run algorithm
+                SumAndMaxDigit(item, ref sum, ref maxDigit);
+
+                // Print output data
+                Console.WriteLine($"Output: sum - {sum}, max digit - {maxDigit}");
+            }
+            Console.WriteLine();
+        }
+        private void SumAndMaxDigit(string input, ref int sum, ref int maxDigit)
+        {
+            sum = 0;
+            maxDigit = 0;
+
+            // Check for possible null value
+            if (input == null)
+            {
+                return;
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                // Check if digit
+                if (char.IsDigit(input[i]))
+                {
+                    int currentDigit = (int)char.GetNumericValue(input[i]);
+                    sum += currentDigit;
+
+                    // Check if currentDigit is greater then maxDigit
+                    if (maxDigit != 9 && currentDigit > maxDigit)
+                    {
+                        maxDigit = currentDigit;
+                    }
+                }
+            }
+        }
+    }
+}

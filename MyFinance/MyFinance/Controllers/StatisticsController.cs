559cb9ab8e132fa1452709b3878e0e9c4f0037c3
Федоренко Володимир <volodymyr.fedorenko@nure.ua>
2023-05-14 00:47:13 +0300

    Implement Statistics page

---
+++ MyFinance/MyFinance/Controllers/StatisticsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyFinance.DB;
+using MyFinance.Models;
+
+namespace MyFinance.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private MyFinanceDbContext _dbContext;
+
+        public StatisticsController(MyFinanceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            SetDates();
+            var statistics = new Statistics(DateTime.Now.Month, DateTime.Now.Year);
+            return View(statistics);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Statistics statistics)
+        {
+            ModelState.Clear();
+            SetDates();
+            statistics.Spendings = _dbContext.Spendings
+                .Include(s => s.Category)
+                .Where(s => s.DateTime.Month == statistics.Month && s.DateTime.Year == statistics.Year)
+                .Select(s => s)
+                .OrderBy(s => s.DateTime)
+                .ToList();
+            return View(statistics);
+        }
+
+        private void SetDates()
+        {
+            SetMonths();
+            SetYears();
+        }
+        private void SetMonths()
+        {
+            List<SelectListItem> months = Enumerable.Range(1, 12)
+                .Select(m => new SelectListItem
+                {
+                    Value = m.ToString(),
+                    Text = m.ToString()
+                }).ToList();
+            ViewData["Months"] = months;
+        }
+        private void SetYears()
+        {
+            List<SelectListItem> years = Enumerable.Range(2010, DateTime.Now.Year - 2010 + 1)
+                .Select(y => new SelectListItem
+                {
+                    Value = y.ToString(),
+                    Text = y.ToString()
+                }).ToList();
+            ViewData["Years"] = years;
+        }
+    }
+}
---
+++ MyFinance/MyFinance/Models/Statistics.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MyFinance.Models
+{
+    public class Statistics
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public List<Spending> Spendings { get; set; }
+        public Statistics()
+        {
+            
+        }
+        public Statistics(int month, int year)
+        {
+            Month = month;
+            Year = year;
+        }
+    }
+}
